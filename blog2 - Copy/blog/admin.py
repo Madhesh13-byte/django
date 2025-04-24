0a66d3a8d46095bbc3f273941caf9c67ef1cd270
Madhesh13-byte <madheshs1303@gmail.com>
2025-04-25 00:30:57 +0530

    Add files via upload

---
+++ blog2 - Copy/blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . import models
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('title','status', 'slug','author')
+admin.site.register(models.Post, AuthorAdmin)
+# Register your models here.
